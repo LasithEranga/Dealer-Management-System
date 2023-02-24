b5831cd47c6da2935eaef2ae55d0e3c6a7740ff0
LasithEranga <lasitheranga1@gmail.com>
2023-02-25 03:01:48 +0530

    fix: sales reports

--- src/app/api/salesReceiptServices.js
+++ src/app/api/salesReceiptServices.js
@@ -3,7 +3,3 @@ import { post } from "./apiManager";
 export const newRecipt = (body, onSuccess, onFailed, onComplete) => {
   post("/sales-receipt/newReceipt", body, onSuccess, onFailed, onComplete);
 };
-
-export const salesReport = (body, onSuccess, onFailed, onComplete) => {
-  post("/sales-receipt/salesReport", body, onSuccess, onFailed, onComplete);
-};
